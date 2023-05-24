37dea6d877ba12713daaf668d22cb3d23f8709bd
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-05-24 17:04:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,7 @@ std::vector<char> getPerm(const Tree& tree, int n) {
   std::string k = tree[n - 1];
   std::vector<char> perm;
   for (int i = 0; i < k.length(); i++) {
-  
+    perm.push_back(k[i]);
   }
   return perm;// напишите реализацию
 }
